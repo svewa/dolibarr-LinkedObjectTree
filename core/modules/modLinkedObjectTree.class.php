6f5ae8050307648fda1dbe9c5bab5ac9fd73e400
svewa <s-codeberg@0j4.de>
2025-12-04 00:52:12 +0100

    proper module id

--- core/modules/modLinkedObjectTree.class.php
+++ core/modules/modLinkedObjectTree.class.php
@@ -41,7 +41,7 @@ class modLinkedObjectTree extends DolibarrModules
 		$this->db = $db;
 
 		// Id for module (must be unique).
-		$this->numero = 500000;
+		$this->numero = 185230;
 		
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'linkedobjecttree';
